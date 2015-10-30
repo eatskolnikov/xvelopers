81150b5d6a9b2eba7b726bef24bccf7558297be8
Enmanuel Toribio <eats007@gmail.com>
2015-10-30 17:38:08 -0400

    Added cllaudiox and fixed links

--- src/app/controllers/homectrl.js
+++ src/app/controllers/homectrl.js
@@ -6,26 +6,41 @@ app.controller("HomeCtrl", ["$scope", "$rootScope", "$window", "$sce", function(
   $scope.xvelopers = [
     {
       name: "Andrés PinedaX",
-      description: "Tiene tanto dinero que la cantidad necesaria para contarlo requiere dos <a href='https://goo.gl/sWTkHd'>bigIntegers</a>",
+      description: "Tiene tanto dinero que la cantidad necesaria para contarlo requiere dos <a href='https://goo.gl/sWTkHd'>bigIntegers</a>.",
       image: "./assets/img/xvelopers/pinedax.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+        {name:"", icon:"uk-icon-facebook", url:"https://goo.gl/rZySlu"}
       ]
     },
-      {
-        name: "Diox",
-        description: "Exiliado del gremio por nuestros pecados, fue el xveloper original.",
-        image: "./assets/img/xvelopers/diox.png",
-        links: [
-          {name:"LinkedIn", icon:"", url:""}
-        ]
-      },
+    {
+      name: "ClaudioX Sanchez",
+      description: "Tiene tanto dinero que la cantidad necesaria para contarlo requiere dos <a href='https://goo.gl/sWTkHd'>bigIntegers</a>.",
+      image: "./assets/img/xvelopers/claudiox.jpg",
+      links: [
+      //  {name:"", icon:"", url:""}
+      ]
+    },
+    {
+      name: "Diox",
+      description: "Exiliado del gremio por nuestros pecados, fue el xveloper original.",
+      image: "./assets/img/xvelopers/diox.png",
+      links: [
+      ]
+    },
     {
       name: "Efrain ReyeX",
-      description: "C# connoisseur extraordinaire",
+      description: "C# connoisseur extraordinaire.",
       image: "./assets/img/xvelopers/efrainreyex.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
+      ]
+    },
+    {
+      name: "Hector MinayaX",
+      description: "Un día, aburrido, se le ocurrió darle al boton de 'nuevo grupo' en facebook y ahora estamos <a href='https://www.facebook.com/groups/devdominicanos/'>aquí</a>.",
+      image: "./assets/img/xvelopers/hectorminayax.jpg",
+      links: [
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
@@ -33,71 +48,71 @@ app.controller("HomeCtrl", ["$scope", "$rootScope", "$window", "$sce", function(
       description: "CEO de como 42 startups, entre ellas <a href='http://codemera.com/'>codemera</a>, <a href='http://coworking.do'>coworking</a> e <a href='http://www.imprimela.net'>imprimela</a>",
       image: "./assets/img/xvelopers/jorgevargax.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
       name: "Leonardo JimeneX",
-      description: "CEO de <a href='http://codetiger.co/'>codetiger</a>",
+      description: "CEO de <a href='http://codetiger.co/'>codetiger</a> y amante de los juegos mobiles.",
       image: "./assets/img/xvelopers/leonardox.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
       name: "Marco MarmolejoX",
-      description: "Hace lo posible cada día por mejorar la industria de videojuegos en República Dominicana",
+      description: "Aficionado/Profesional del diseño de videojuegos. Hace lo posible cada día por mejorar <a href='https://www.facebook.com/groups/cddvorg/'>la industria de videojuegos en República Dominicana</a>.",
       image: "./assets/img/xvelopers/marcomarmolejox.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
-      name: "Raydelto HernandeX",
-      description: "Fiel amante de Blackberry y profesor de vocación, ha dado clases en todas las universidades.",
+      name: "Omar DuarteX",
+      description: "El pana mas easy going, se fue para <a href='http://www.workinestonia.com/'>Estonía</a> con un trabajo remoto que se transformó en onsite.",
+      image: "./assets/img/xvelopers/marcomarmolejox.jpg",
+      links: [
+      //  {name:"", icon:"", url:""}
+      ]
+    },
+    {
+      name: "RaydeltoX Hernandez",
+      description: "Fiel creyente de Blackberry y profesor de vocación, ha dado clases en todas las universidades y en sus tiempos libres escribe <a href='http://amzn.com/1785283839'>libros</a>.",
       image: "./assets/img/xvelopers/raydeltox.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
       name: "René OlivoX",
-      description: "",
+      description: "#LaVidaEnZen el más #vendezido, actualmente se la pasa de charla en charla y lo puedes encontrar en Bräu un día cualquiera en #chillax.",
       image: "./assets/img/xvelopers/renex.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
-      name: "Riaz HemmatX",
-      description: "Constantemente aboga por mejorar el mercado laboral de los desarrolladores a travez de su compañia <a href='http://www.bluecoding.com/'>bluecoding</a>",
+      name: "RiaX Hemmat",
+      description: "Constantemente aboga por mejorar el mercado laboral de los desarrolladores por medio de su compañia <a href='http://www.bluecoding.com/'>bluecoding</a>.",
       image: "./assets/img/xvelopers/riazhermmatx.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
       name: "Rubens MariuzzoX",
-      description: "",
+      description: "Un pana bien, es tan bacano que cuando consiguió un trabajo remoto con <a href='http://x-team.com/'>X-TEAM</a> (not related to xvelopers), se mudó a Pedernales.",
       image: "./assets/img/xvelopers/rubensmariuzzox.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     },
     {
-      name: "Victor RecioX",
-      description: "",
-      image: "./assets/img/xvelopers/victorx.jpg",
-      links: [
-        {name:"LinkedIn", icon:"", url:""}
-      ]
-    },
-    {
-      name: "Vivian GuillenX",
-      description: "",
+      name: "VivianX Guillen",
+      description: "Co-Fundadora de <a href='http://codetiger.co/'>codetiger</a>, se graduó econmía y se lo encontró demasiado aburrido, así que ahora trabaja en python. No le digas nada malo de <a href='http://getuikit.com/'>uikit</a>.",
       image: "./assets/img/xvelopers/vivianx.jpg",
       links: [
-        {name:"LinkedIn", icon:"", url:""}
+      //  {name:"", icon:"", url:""}
       ]
     }];
   }]);
