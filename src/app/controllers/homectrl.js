9768b1acc6776ee8a3d18a98915ac61be6797d09
Enmanuel Toribio <eats007@gmail.com>
2015-11-17 17:15:53 -0400

    Added orderBy filter so the list will be always sorted

--- src/app/controllers/homectrl.js
+++ src/app/controllers/homectrl.js
@@ -1,12 +1,13 @@
-app.controller("HomeCtrl", ["$sce", "$http", function($sce, $http)
+app.controller("HomeCtrl", ["$sce", "$http", "$filter", function($sce, $http, $filter)
 {
+  var orderBy = $filter('orderBy');
+  this.xvelopers =[];
   this.giveMeHtml = function(text){
     return $sce.trustAsHtml(text);
   };
-  this.xvelopers =[];
   this.init = function(){
     $http.get('./xvelopers.json').success(angular.bind(this, function(data){
-      this.xvelopers = data;
+      this.xvelopers = orderBy(data,'name',false);
     }));
   };
   this.init();
