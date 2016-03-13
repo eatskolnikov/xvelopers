a7f3454b706fac83faaf10c7beaf39c23512848d
Enmanuel Toribio <eats007@gmail.com>
2016-03-12 23:48:08 -0400

    Added PSA and some design stuff

--- src/react_components/xvelopers.jsx
+++ src/react_components/xvelopers.jsx
@@ -22,9 +22,21 @@ var XvelopersList = React.createClass({
           <a href={link.url} className={className} target="_blank"></a>
         );
       };
-      function xveloperDescription() { return {__html: xveloper.description}; }
+      function xveloperDescription() {
+        if(xveloper.description.length > 100){
+          var stringLength = 50;
+          if(xveloper.description.indexOf("<a") > -1 && xveloper.description.indexOf("<a") <= stringLength){
+            stringLength = xveloper.description.indexOf("<a");
+          }
+          var shown_part = xveloper.description.substr(0,stringLength);
+          var hidden_part = "<span class='uk-hidden' id='"+xveloper.id+"-more'>" + xveloper.description.substr(stringLength) + "</span>";
+          var showm_more_btn = " <small> <a href='#' data-uk-toggle=\"{target:'#"+xveloper.id+"-more'}\">mostrar más</a> </small>";
+          xveloper.description = shown_part+hidden_part+showm_more_btn;
+        }
+        return {__html: xveloper.description};
+      }
       return (
-        <div className="uk-width-medium-1-3 uk-text-center uk-margin-top" id="{xveloper.id}">
+        <div className="uk-width-medium-1-4 uk-text-center uk-margin-top" id="{xveloper.id}">
             <div className="uk-thumbnail uk-overlay-hover uk-border-circle" >
               <figure className="uk-overlay">
                 <img className="uk-border-circle" width="240" height="240" src={xveloper.image} />
@@ -35,7 +47,7 @@ var XvelopersList = React.createClass({
                 </figcaption>
               </figure>
             </div>
-            <h2 className="uk-margin-bottom-remove">{xveloper.name}</h2>
+            <h3 className="uk-margin-bottom-remove">{xveloper.name}</h3>
             <p className="uk-text-large uk-margin-top-remove uk-text-muted" dangerouslySetInnerHTML={xveloperDescription()}></p>
           </div>
       );
